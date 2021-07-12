4b2b11b461b7a2fbf76ef34ae13814a57b3585ef
developerjhp <developerjhp@gmail.com>
2021-07-12 20:23:55 +0900

    src/Pages/Mypage.js

--- src/Pages/MyPage.js
+++ src/Pages/MyPage.js
@@ -4,8 +4,8 @@ import Tweet from '../Components/Tweet';
 import './MyPage.css';
 import dummyTweets from '../static/dummyData';
 
-const MyPage = () => {
-  const filteredTweets = dummyTweets;
+const MyPage = (props) => {
+  const filteredTweets = dummyTweets.filter( (el)=>el.username === 'parkhacker')
   // TODO : 주어진 트윗 목록(dummyTweets)중 현재 유져인 parkhacker의 트윗만 보여줘야 합니다.
 
   return (
@@ -13,19 +13,25 @@ const MyPage = () => {
       <div className="myInfo__container">
         <div className="myInfo__wrapper">
           <div className="myInfo__profile">
-            <img src={filteredTweets[1].picture} />
+            <img src={filteredTweets[0].picture} />
           </div>
           <div className="myInfo__detail">
             <p className="myInfo__detailName">
-              {filteredTweets[1].username} Profile
+              {filteredTweets[0].username} Profile
             </p>
             <p>28 팔로워 100 팔로잉</p>
           </div>
         </div>
       </div>
-      <ul className="tweets__mypage">
-        <Tweet tweet={filteredTweets[0]}/>
-        {/* TODO : 주어진 트윗 목록(dummyTweets)중 현재 유져인 parkhacker의 트윗만 보여줘야 합니다. */}
+      <ul className="tweets__mypage" >
+      {filteredTweets.map((e) => {
+        
+        return (
+        <Tweet tweet={e} key={e.id}/>
+        )
+      }
+      )
+    }
       </ul>
       <Footer />
     </section>
